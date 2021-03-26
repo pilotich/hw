c20e3fbefba6af92ee1b72635285ff91c20f255b
pilotich <72825010+pilotich@users.noreply.github.com>
2021-03-26 11:30:35 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-const name = prompt('Enter your name?');
-alert(name);
+//const name = prompt('Enter your name?');
+//alert(name);
 
 let i = 0;
 
 while (i === 0){
     let number = prompt('write your number...');
-    let result = Math.pow(+number, 2);
-    let square = prompt('square this numbers');
-    if (+square === +result){
-        alert('you are absolutely right');
-        i++;
+
+    if(!isNaN(Number(number)) && number !== '') {
+
+        let result = Math.pow(+number, 2);
+        let square = prompt('square this numbers');
+
+        if (+square === +result) {
+            alert('you are absolutely right');
+            i++;
+        }
+
+        else {
+            alert('oh no, you miss it. Try again ');
+        }
     }
+
     else{
-        alert('oh no, you miss it. Try again ');
+         alert('That is not what i need, try again');
     }
 }
